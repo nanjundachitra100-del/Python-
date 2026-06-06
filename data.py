acd7d1013d882063f614cb4a23541d8db55d3880
ChitraShree Gowda <nanjundachitra100@gmail.com>
2026-06-06 20:27:33 +0530

    Added NumPy practice programs and cleaned up text file

---
+++ data.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+a=np.array([1,2,3,4,5,6]),
+b=np.array([10,20,30,40,50,60])
+
+print("ADDITION:",a+b)
+print("SUBTRACTION:",a-b)
+print("MULTIPLICATION:",a*b)
+print("DIVISION:",a/b)
+
+print("original a:",a)
+print("original b:",b)
+
