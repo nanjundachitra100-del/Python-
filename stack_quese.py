a778ab35e24003240260c06abc487cfc320e886f
ChitraShree Gowda <nanjundachitra100@gmail.com>
2026-06-13 13:47:18 +0530

    added queue concets

--- stack_quese.py
+++ stack_quese.py
@@ -14,3 +14,22 @@ print(stack)
 stack.pop()
 stack.pop()
 print(len(stack)==0)
+
+#queue
+from collections import deque
+queue=[1,2,3,4]
+queue.append(5)
+queue.append(6)
+print(queue)
+queue.popleft()
+print(queue)
+queue.popleft()
+queue.popleft()
+queue.popleft()
+
+#check if queue is empty or not
+print(len(queue)==0)
+print(queue)
+queue.popleft()
+queue.popleft()
+print(len(queue)==0)
