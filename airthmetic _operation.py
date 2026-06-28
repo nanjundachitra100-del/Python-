3f8f3d4018fc67a4d67f0f321902c666a21d9e40
ChitraShree Gowda <nanjundachitra100@gmail.com>
2026-06-28 09:52:22 +0530

    basic arithmetic problums

---
+++ airthmetic _operation.py
@@ -0,0 +1,25 @@
+#addition
+num1=10
+num2=7
+sum=num1+num2
+print(f"addition :{num1}+{num2}={sum}")
+
+#subtraction
+difference=num1-num2
+print(f"subtraction:{num1}-{num2}={difference}")
+
+#multiplicatin
+product=num1*num2
+print(f"product:{num1}*{num2}={product}")
+
+#floor dividion
+fdiv=num1//num2
+print(f"floor division:{num1}//{num2}={fdiv}")
+
+#power
+exponential=num1**num2
+print(f"{num1}**{num2}={exponential}")
+
+#modulus
+modulus=num1%num2
+print(f"modulus:{num1}%{num2}={modulus}")
