fa041900e7c2a1d1fc8b4828df3c591145761c35
ChitraShree Gowda <nanjundachitra100@gmail.com>
2026-06-28 10:35:59 +0530

    control statments

---
+++ control_statments.py
@@ -0,0 +1,24 @@
+#if statments
+n=int(input("Enter a number:"))
+if n>0:
+    print("n is positive")
+else:
+    print("n is negative")
+
+#if elif else statments
+Marks=int (input("Enter the marks:"))
+
+if Marks>90:
+    print("secured A grade")
+elif(Marks>80):
+    print("secured B grade")
+elif(Marks>70):
+    print("secured C grade")
+else:
+    print("secured D grade")
+
+# while loop
+i=1
+while i<=5:
+    print(i)
+    i+=1
